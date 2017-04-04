7635c8b797ecd03bed87377545d48d5531e5c3f9
Supsane <echashurin@gmail.com>
2017-04-04 15:14:20 +0400

    Complete

--- app/src/main/java/com/example/chashurinevgeny/intheanimalworld/Controller.java
+++ app/src/main/java/com/example/chashurinevgeny/intheanimalworld/Controller.java
@@ -12,16 +12,21 @@ import android.widget.TextView;
 public class Controller {
     private int i = 1;
     private int correctAnswer = 0;
-    java.util.Formatter formatter = new java.util.Formatter();
 
     public void clickHandler(View view, Context context, TextView startTextView, TextView questionsLeftNumber, TextView questionsCompleteNumber, Button trueButton, Button falseButton, Button restartButton, Button exitButton) {
         String[] questions = context.getResources().getStringArray(R.array.questions);
+        String trueText = context.getResources().getString(R.string.trueButton);
+        String falseText = context.getResources().getString(R.string.falseButton);
+        String[] answer = context.getResources().getStringArray(R.array.answer);
         if (view.getId() == R.id.startButton || view.getId() == R.id.restartButton) {
             startTextView.setText(questions[0]);
             questionsLeftNumber.setText(String.valueOf(questions.length));
         }
         if (view.getId() == R.id.trueButton || view.getId() == R.id.falseButton) {
             if (i >= questions.length) {
+                if (view.getId() == R.id.trueButton && trueText.equals(answer[i-1])) correctAnswer++;
+                if (view.getId() == R.id.falseButton && falseText.equals(answer[i-1])) correctAnswer++;
+                java.util.Formatter formatter = new java.util.Formatter();
                 trueButton.setVisibility(View.INVISIBLE);
                 falseButton.setVisibility(View.INVISIBLE);
                 restartButton.setVisibility(View.VISIBLE);
@@ -32,15 +37,13 @@ public class Controller {
                 questionsLeftNumber.setText(String.valueOf(questions.length - i));
                 i = 1;
                 correctAnswer = 0;
+                formatter.close();
             } else {
+                if (view.getId() == R.id.trueButton && trueText.equals(answer[i-1])) correctAnswer++;
+                if (view.getId() == R.id.falseButton && falseText.equals(answer[i-1])) correctAnswer++;
                 startTextView.setText(questions[i]);
                 questionsCompleteNumber.setText(String.valueOf(i));
                 questionsLeftNumber.setText(String.valueOf(questions.length - i));
-                String trueText = context.getResources().getString(R.string.trueButton);
-                String falseText = context.getResources().getString(R.string.falseButton);
-                String[] answer = context.getResources().getStringArray(R.array.answer);
-                if (view.getId() == R.id.trueButton && trueText.equals(answer[i])) correctAnswer++;
-                if (view.getId() == R.id.falseButton && falseText.equals(answer[i])) correctAnswer++;
                 i++;
             }
         }
