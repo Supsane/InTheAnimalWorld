4e1a99742ae9f6ab2225b563e7c5fe1785167af9
Supsane <echashurin@gmail.com>
2017-04-04 14:27:50 +0400

    Почти доделан подсчет правильных ответов

--- app/src/main/java/com/example/chashurinevgeny/intheanimalworld/Controller.java
+++ app/src/main/java/com/example/chashurinevgeny/intheanimalworld/Controller.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 
 public class Controller {
     private int i = 1;
+    private int correctAnswer = 0;
+    java.util.Formatter formatter = new java.util.Formatter();
 
     public void clickHandler(View view, Context context, TextView startTextView, TextView questionsLeftNumber, TextView questionsCompleteNumber, Button trueButton, Button falseButton, Button restartButton, Button exitButton) {
         String[] questions = context.getResources().getStringArray(R.array.questions);
@@ -19,19 +21,26 @@ public class Controller {
             questionsLeftNumber.setText(String.valueOf(questions.length));
         }
         if (view.getId() == R.id.trueButton || view.getId() == R.id.falseButton) {
-            if(i >= questions.length) {
+            if (i >= questions.length) {
                 trueButton.setVisibility(View.INVISIBLE);
                 falseButton.setVisibility(View.INVISIBLE);
                 restartButton.setVisibility(View.VISIBLE);
                 exitButton.setVisibility(View.VISIBLE);
-                startTextView.setText(R.string.finishTextView);
+                String finishTextView = context.getResources().getString(R.string.finishTextView);
+                startTextView.setText(String.valueOf(formatter.format(finishTextView, String.valueOf(correctAnswer))));
                 questionsCompleteNumber.setText(String.valueOf(i));
                 questionsLeftNumber.setText(String.valueOf(questions.length - i));
                 i = 1;
+                correctAnswer = 0;
             } else {
                 startTextView.setText(questions[i]);
                 questionsCompleteNumber.setText(String.valueOf(i));
                 questionsLeftNumber.setText(String.valueOf(questions.length - i));
+                String trueText = context.getResources().getString(R.string.trueButton);
+                String falseText = context.getResources().getString(R.string.falseButton);
+                String[] answer = context.getResources().getStringArray(R.array.answer);
+                if (view.getId() == R.id.trueButton && trueText.equals(answer[i])) correctAnswer++;
+                if (view.getId() == R.id.falseButton && falseText.equals(answer[i])) correctAnswer++;
                 i++;
             }
         }
