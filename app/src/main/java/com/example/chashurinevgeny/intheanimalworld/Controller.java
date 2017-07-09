d0bdb8220ea70eb2686d98a0ad0d61f8e53dd6c7
Supsane <echashurin@gmail.com>
2017-07-09 16:00:56 +0300

    easy correct Controller

--- app/src/main/java/com/example/chashurinevgeny/intheanimalworld/Controller.java
+++ app/src/main/java/com/example/chashurinevgeny/intheanimalworld/Controller.java
@@ -9,11 +9,11 @@ import android.widget.TextView;
  * Created by Chashurin Evgeny on 03.04.2017.
  */
 
-public class Controller {
+class Controller {
     private int i = 1;
     private int correctAnswer = 0;
 
-    public void clickHandler(View view, Context context, TextView startTextView, TextView questionsLeftNumber, TextView questionsCompleteNumber, Button trueButton, Button falseButton, Button restartButton, Button exitButton) {
+    void clickHandler(View view, Context context, TextView startTextView, TextView questionsLeftNumber, TextView questionsCompleteNumber, Button trueButton, Button falseButton, Button restartButton, Button exitButton) {
         String[] questions = context.getResources().getStringArray(R.array.questions);
         String trueText = context.getResources().getString(R.string.trueButton);
         String falseText = context.getResources().getString(R.string.falseButton);
@@ -24,23 +24,28 @@ public class Controller {
         }
         if (view.getId() == R.id.trueButton || view.getId() == R.id.falseButton) {
             if (i >= questions.length) {
-                if (view.getId() == R.id.trueButton && trueText.equals(answer[i-1])) correctAnswer++;
-                if (view.getId() == R.id.falseButton && falseText.equals(answer[i-1])) correctAnswer++;
+                if (view.getId() == R.id.trueButton && trueText.equals(answer[i - 1]))
+                    correctAnswer++;
+                if (view.getId() == R.id.falseButton && falseText.equals(answer[i - 1]))
+                    correctAnswer++;
                 java.util.Formatter formatter = new java.util.Formatter();
                 trueButton.setVisibility(View.INVISIBLE);
                 falseButton.setVisibility(View.INVISIBLE);
                 restartButton.setVisibility(View.VISIBLE);
                 exitButton.setVisibility(View.VISIBLE);
                 String finishTextView = context.getResources().getString(R.string.finishTextView);
-                startTextView.setText(String.valueOf(formatter.format(finishTextView, String.valueOf(correctAnswer))));
+                startTextView.setText(String.valueOf(formatter.format(finishTextView, String
+                        .valueOf(correctAnswer))));
                 questionsCompleteNumber.setText(String.valueOf(i));
                 questionsLeftNumber.setText(String.valueOf(questions.length - i));
                 i = 1;
                 correctAnswer = 0;
                 formatter.close();
             } else {
-                if (view.getId() == R.id.trueButton && trueText.equals(answer[i-1])) correctAnswer++;
-                if (view.getId() == R.id.falseButton && falseText.equals(answer[i-1])) correctAnswer++;
+                if (view.getId() == R.id.trueButton && trueText.equals(answer[i - 1]))
+                    correctAnswer++;
+                if (view.getId() == R.id.falseButton && falseText.equals(answer[i - 1]))
+                    correctAnswer++;
                 startTextView.setText(questions[i]);
                 questionsCompleteNumber.setText(String.valueOf(i));
                 questionsLeftNumber.setText(String.valueOf(questions.length - i));
